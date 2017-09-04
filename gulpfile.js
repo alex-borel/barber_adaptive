8f8337330c6fa27fad86b42cc3dc63af7fab656d
Alexander Borel <alex.b.minsk@gmail.com>
2017-09-04 16:39:21 +0300

    barber/feature: add main grid

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var server = require('browser-sync').create();
+
+
+gulp.task('serve', function () {
+  server.init({
+    server: './',
+    notify: false,
+    open: true,
+    ui: false
+  });
+
+  gulp.watch('sass/**/*.scss', function(event, cb) {
+    setTimeout(function() {gulp.start('styles');},500)
+  });
+  
+  gulp.watch('*.html').on('change', server.reload);
+});
+
+gulp.task('styles', function () {
+     gulp.src('./sass/style.scss')
+    .pipe(sass().on('error', sass.logError))    
+    .pipe(gulp.dest('./css/'))
+    .pipe(server.reload({
+      stream: true
+    }))
+});
+
+gulp.task('default', ['serve']);
+
